befe38a43f2a4f625bf8e1dd86005c81fb401e40
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-01-10 00:35:51 +0100

    refactor(yatzy1): :recycle: move class __init__ to top

--- src/yatzy1.py
+++ src/yatzy1.py
@@ -1,5 +1,13 @@
 class Yatzy:
 
+    def __init__(self, d1=0, d2=0, d3=0, d4=0, _5=0):
+        self.dice = [0] * 5
+        self.dice[0] = d1
+        self.dice[1] = d2
+        self.dice[2] = d3
+        self.dice[3] = d4
+        self.dice[4] = _5
+
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
         total = 0
@@ -65,14 +73,6 @@ class Yatzy:
         if (d5 == 3):
             s += 3
         return s
-
-    def __init__(self, d1=0, d2=0, d3=0, d4=0, _5=0):
-        self.dice = [0] * 5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
 
     def fours(self):
         sum = 0
