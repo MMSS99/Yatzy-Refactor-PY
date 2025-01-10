6b569e575574232649adfe2a75e1394894e2961b
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-01-10 03:55:48 +0100

    refactor(yatzy1-test): :recycle: change x_of_a_kind function tests to give arguments to class instead of function

--- test/test_yatzy1.py
+++ test/test_yatzy1.py
@@ -55,11 +55,11 @@ def test_sixes_test():
     assert 6 == Yatzy(4, 4, 6, 5, 5).sixes()
     assert 18 == Yatzy(6, 5, 6, 6, 5).sixes()
 
-
+# Now the arguments must be given to the class
 def test_one_pair():
-    assert 6 == Yatzy().score_pair(3, 4, 3, 5, 6)
-    assert 10 == Yatzy().score_pair(5, 3, 3, 3, 5)
-    assert 12 == Yatzy().score_pair(5, 3, 6, 6, 5)
+    assert 6 == Yatzy(3, 4, 3, 5, 6).score_pair()
+    assert 10 == Yatzy(5, 3, 3, 3, 5).score_pair()
+    assert 12 == Yatzy(5, 3, 6, 6, 5).score_pair()
 
 
 def test_two_Pair():
@@ -69,16 +69,16 @@ def test_two_Pair():
 
 
 def test_three_of_a_kind():
-    assert 9 == Yatzy().three_of_a_kind(3, 3, 3, 4, 5)
-    assert 15 == Yatzy().three_of_a_kind(5, 3, 5, 4, 5)
-    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
+    assert 9 == Yatzy(3, 3, 3, 4, 5).three_of_a_kind()
+    assert 15 == Yatzy(5, 3, 5, 4, 5).three_of_a_kind()
+    assert 9 == Yatzy(3, 3, 3, 3, 5).three_of_a_kind()
 
 
 def test_four_of_a_knd():
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
-    assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
-    assert 0 == Yatzy.four_of_a_kind(3, 3, 3, 2, 1)
+    assert 12 == Yatzy(3, 3, 3, 3, 5).four_of_a_kind()
+    assert 20 == Yatzy(5, 5, 5, 4, 5).four_of_a_kind()
+    assert 12 == Yatzy(3, 3, 3, 3, 3).four_of_a_kind()
+    assert 0 == Yatzy(3, 3, 3, 2, 1).four_of_a_kind()
 
 
 def test_smallStraight():
